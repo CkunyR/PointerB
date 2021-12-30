887575298699e2bcbe19ad465c2536cf63368b79
Zhenkun Guo <1608229093@qq.com>
2021-12-30 22:24:38 +0800

    利用指针求元素个数

---
+++ Pointer.c
@@ -0,0 +1,23 @@
+//利用指针求元素个数
+
+#include <stdio.h>
+
+int my_strlen(char* str)
+{
+	char* start = str;
+	char* end = str;
+	while (*end != '\0')
+	{
+		end++;
+	}
+	return end - start;
+}
+
+int main()
+{
+	char arr[10] = "bit";
+	int len = my_strlen(arr);
+	printf("%d\n", len);
+
+	return 0;
+}
